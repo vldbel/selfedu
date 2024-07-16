dd8a8776c52195adf3a6d7220978db07e30b94b9
vldbel <vldbel@gmail.com>
2024-07-16 16:10:41 +0000

    2-2-8

--- selfedu_oop/2-2-property/selfedu_2-2-8.py
+++ selfedu_oop/2-2-property/selfedu_2-2-8.py
@@ -8,7 +8,7 @@ class TreeObj:
         
     @property
     def left(self):
-        return self.left
+        return self.__left
     
     @left.setter
     def left(self, obj):
@@ -16,7 +16,7 @@ class TreeObj:
     
     @property
     def right(self):
-        return self.right
+        return self.__right
     
     @right.setter
     def right(self, obj):
@@ -25,11 +25,36 @@ class TreeObj:
 
 class DecisionTree:
     """для работы с решающим деревом в целом"""
+    def __init__(self) -> None:
+        pass
     
     @classmethod
     def predict(cls, root, x):
-        ...
-    
+        if x:
+            obj:TreeObj = root
+            while not obj.value:
+                obj = obj.left if x[obj.indx] else obj.right
+            return obj.value 
+        
     @classmethod
-    def add_obj(cls, obj, node=None, left=True):
-        ...
+    def add_obj(cls, obj, node:TreeObj=None, left=True):            
+        if node is not None and isinstance(node, TreeObj): # part of branches or a leaf
+            if left:
+                node.left = obj
+            else:  # right
+                node.right = obj
+        return obj
+        
+                  
+
+root = DecisionTree.add_obj(TreeObj(0))
+v_11 = DecisionTree.add_obj(TreeObj(1), root)
+v_12 = DecisionTree.add_obj(TreeObj(2), root, False)
+DecisionTree.add_obj(TreeObj(-1, "будет программистом"), v_11)
+DecisionTree.add_obj(TreeObj(-1, "будет кодером"), v_11, False)
+DecisionTree.add_obj(TreeObj(-1, "не все потеряно"), v_12)
+DecisionTree.add_obj(TreeObj(-1, "безнадежен"), v_12, False)
+
+
+x = [1, 1, 0]
+res = DecisionTree.predict(root, x) # будет программистом
