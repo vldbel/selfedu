ee3faa8b88e0beed2437417f19141e208d783f73
vldbel <vldbel@gmail.com>
2024-10-07 19:39:08 +0000

    3.4.2 update

--- selfedu_oop/Section_3/3-4-add_sub/selfedu_3-4-2.py
+++ selfedu_oop/Section_3/3-4-add_sub/selfedu_3-4-2.py
@@ -1,77 +1,81 @@
+"""Module dockstring"""
+
 class ListMath:
+    """Test class"""
     LIST_ALLOWED_TYPES = [int, float]
-    
-    def __init__(self, lst=None):
+
+    def __init__(self, inlst=None):
         # self.lst_math = self.filter_types(lst) if lst and type(lst) == list else []
-        if not lst:
+        if not inlst:
             self.lst_math = []
-        else: 
-            self.lst_math = self.filter_types(lst)
-        
+        else:
+            self.lst_math = self.filter_types(inlst)
+
     @classmethod
-    def filter_types(cls, lst:list):
-        return [item for item in lst if type(item) in cls.LIST_ALLOWED_TYPES]
+    def filter_types(cls, l:list):
+        """check that items in list are in LIST_ALLOWED_TYPES"""
+        return [item for item in l if type(item) in cls.LIST_ALLOWED_TYPES]
 
     @classmethod
     def check_value_type(cls, value):
+        """chehcs if value in cls.LIST_ALLOWED_TYPES"""        
         if type(value) not in cls.LIST_ALLOWED_TYPES:
             raise ArithmeticError("Not allowed value")
-        else:
-            return True
-    
+        return True
+
     def __str__(self) -> str:
         return str(self.lst_math)
-    
+
     def __add__(self, item):
         self.check_value_type(item)
         return self.__class__(map(lambda x: x + item, self.lst_math))
-        
+
     def __radd__(self, item):
         return self.__add__(item)
-    
+
     def __iadd__(self, item):
-        for i in range(len(self.lst_math)):
+        for i, _ in enumerate(self.lst_math):
             self.lst_math[i] += item
         return self
-    
+
     def __sub__(self, item):
         self.check_value_type(item)
         return self.__class__(map(lambda x: x - item, self.lst_math))
-        
+
     def __rsub__(self, item):
         self.check_value_type(item)
-        return self.__class__(map(lambda x: item - x, self.lst_math))        
-    
+        return self.__class__(map(lambda x: item - x, self.lst_math))
+
     def __isub__(self, item):
-        for i in range(len(self.lst_math)):
+        for i, _ in enumerate(self.lst_math):
             self.lst_math[i] -= item
         return self
 
     def __mul__(self, item):
         self.check_value_type(item)
         return self.__class__(map(lambda x: x * item, self.lst_math))
-        
+
     def __rmul__(self, item):
         return self.__mul__(item)
-    
+
     def __imul__(self, item):
-        for i in range(len(self.lst_math)):
+        for i, _ in enumerate(self.lst_math):
             self.lst_math[i] *= item
         return self
 
     def __truediv__(self, item):
         self.check_value_type(item)
         return self.__class__(map(lambda x: x / item, self.lst_math))
-        
+
     def __rtruediv__(self, item):
         return self.__class__(map(lambda x: item / x, self.lst_math))
-        
+
     def __itruediv__(self, item):
-        for i in range(len(self.lst_math)):
+        for i, _ in enumerate(self.lst_math):
             self.lst_math[i] /= item
         return self
 
-    
+
 lst1 = ListMath() # пустой список
 lst2 = ListMath([1, 2, -5, 7.68]) # список с начальными значениями
 lst = ListMath([1, "abc", -5, 7.68, True]) # ListMath: [1, -5, 7.68]
