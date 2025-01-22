93cbdf3a8aa583304cda9ee791bc612f642e501f
Vladimir <vldbel@gmail.com>
2025-01-22 22:48:30 +0200

    from minsk

--- selfedu_oop/section_4/4-7_slots/selfedu_4-7-8.py
+++ selfedu_oop/section_4/4-7_slots/selfedu_4-7-8.py
@@ -18,6 +18,40 @@ class Function:
         obj = self.__class__(self)
         obj._bias = self._bias + other
         return obj
-    
+
+    def __mul__(self, other):
+        if type(other) not in (int, float):
+            raise TypeError('амплитуда должна быть числом')
+
+        obj = self.__class__(self)
+        obj._amplitude = self._amplitude * other
+        return obj        
+
+
 class Linear(Function):
-    
+    def __init__(self, *args):
+        super().__init__()
+        self._k, self._b = (args[0], args[1]) if len(args) == 2 else (args[0]._k, args[0]._b)
+                    
+    def _get_function(self, x):
+        return self._k * x + self._b
+        
+         
+f = Linear(1, 0.5)
+print(f.__dict__)
+
+f2 = f + 10   # изменение смещения (атрибут _bias)
+print(f2.__dict__)
+
+y1 = f(0)     # 0.5
+print(y1)
+
+y2 = f2(0)    # 10.5
+print(y2)
+
+f = Linear(1, 0.5)
+f2 = f * 5    # изменение амплитуды (атрибут _amplitude)
+y1 = f(0)     # 0.5
+print(y1)
+y2 = f2(0)    # 2.5
+print(y2)
